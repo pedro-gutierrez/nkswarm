93ac013a83b2b3b7ca28233a4e8879f0e43cdd53
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-07-27 13:19:45 +0200

    improved stats for cluster health

--- src/nkswarm.erl
+++ src/nkswarm.erl
@@ -1,25 +1,19 @@
 -module(nkswarm).
 -export([status/0, metrics/1]).
+-define(HEALTH_COLORS, [red, yellow, green]).
 
 status() ->
     Timeout = nkswarm_config:timeout(),
     gen_server:call(nkswarm_server, status, Timeout+2500).
 
 metrics(specs) ->
-    [#{ type => gauge, 
-        name => erlang_cluster_size, 
-        help => "The size of the erlang cluster" }, 
-     #{ type => gauge,
-        name => erlang_cluster_health_status,
-        labels => [color],
-        help => "The health of the erlang cluster status (red, yellow, green)"}];
+    [{gauge, erlang_cluster_size, "The size of the erlang cluster"}, 
+     {gauge, erlang_cluster_health_status, [color], "The health of the erlang cluster status (red, yellow, green)"}];
 
 metrics(values) ->
     #{ nodes := Nodes, status := Color } = status(),
-    [#{ type => gauge, 
-        name => erlang_cluster_size, 
-        value =>  length(Nodes) }, 
-     #{ type => gauge,
-        name => erlang_cluster_health_status,
-        labels => [Color],
-        value => 1}].
+    [{gauge, erlang_cluster_size, length(Nodes)} | health_status_metrics(Color)].
+
+health_status_metrics(Color) ->
+    [{gauge, erlang_cluster_health_status, [Color], 1} | 
+     [{gauge, erlang_cluster_health_status, [C], 0} || C <- ?HEALTH_COLORS, C =/= Color ]].
