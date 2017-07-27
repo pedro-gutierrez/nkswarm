4540a9cd214a6a62d77f976d038169d68df9d7f4
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-07-27 11:15:44 +0200

    added nkstats metrics

--- src/nkswarm.erl
+++ src/nkswarm.erl
@@ -1,6 +1,17 @@
 -module(nkswarm).
--export([status/0]).
+-export([status/0, metrics/1]).
 
 status() ->
     Timeout = nkswarm_config:timeout(),
     gen_server:call(nkswarm_server, status, Timeout+2500).
+
+metrics(specs) ->
+    [#{ type => gauge, 
+        name => erlang_cluster_size, 
+        help => "The size of the erlang cluster" }];
+
+metrics(values) ->
+    #{ nodes := Nodes } = status(),
+    [#{ type => gauge, 
+        name => erlang_cluster_size, 
+        value =>  length(Nodes) }].
