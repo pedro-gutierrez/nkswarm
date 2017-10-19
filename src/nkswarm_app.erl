6aa4ce24d60fd8e3b3d1e3b250df72f006d3fc25
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-10-19 22:43:22 +0200

    moving to nkservice

--- src/nkswarm_app.erl
+++ src/nkswarm_app.erl
@@ -1,9 +1,30 @@
 -module(nkswarm_app).
 -behaviour(application).
 -export([start/2, stop/1]).
+-define(APP, sipstorm_c4).
 
 start(_, _) ->
-    nkswarm_sup:start_link().
+    Syntax = #{
+      discovery_port => {integer, 1, 65535},
+      discovery_name => atom,
+      beacon_timeout => integer,
+      beacon_interval => integer,
+      '__defaults' => #{
+        discovery_port => 9999,
+        beacon_timeout => 1000,
+        beacon_interval => 300
+       },
+      '__mandatory' => [discovery_name]
+     },
+    case nklib_config:load_env(?APP, Syntax) of
+        {ok, _} ->
+            {ok, Pid} = nkswarm_sup:start_link(),
+            {ok, Vsn} = application:get_key(?APP, vsn),
+            {ok, Pid};
+        error, Error} ->
+            lager:error("Error parsing config: ~p", [Error]),
+            error(Error)
+    end.    
 
 stop(_) ->
     ok.
